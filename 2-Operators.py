ed564a4e40a22fbf812dbfc9ba340475c582a9a1
Jingchun Wang <bettyjcwang@gmail.com>
2025-10-07 16:28:26 +0100

    Implement meal cost calculation with tip and tax
    Refactor meal cost calculation into a solve function.

--- 2-Operators.py
+++ 2-Operators.py
@@ -1,19 +1,32 @@
-def get_total_cost_of_meal():
-    # original meal price
-    meal_cost = float(input())
-    # tip percentage
-    tip_percent = int(input())
-    # tax percentage
-    tax_percent = int(input())
+#!/bin/python3
 
-    # Write your calculation code here
-    tip = # calculate tip
-    tax = # caclulate tax
+import math
+import os
+import random
+import re
+import sys
 
-    # cast the result of the rounding operation to an int and save it as total_cost 
-    total_cost = int(round("""write your total cost calculation here"""))
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+
+    tip = meal_cost * tip_percent / 100
     
-    return str(total_cost)
+    tax = meal_cost * tax_percent / 100
+    
+    total_cost = meal_cost + tip + tax
+    
+    print(round(total_cost))
 
-# Print your result
-print("The total meal cost is " + get_total_cost_of_meal() + " dollars.")
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+    tip_percent = int(input().strip())
+    tax_percent = int(input().strip())
+    solve(meal_cost, tip_percent, tax_percent)
