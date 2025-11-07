7285cdae15e55459f97e94cab9febfb632c60471
Jingchun Wang <bettyjcwang@gmail.com>
2025-11-07 19:06:51 +0000

    Refactor conditional logic in main execution block
    Refactor conditional statements for clarity and structure.

--- 3-Intro to conditional statements.py
+++ 3-Intro to conditional statements.py
@@ -1,11 +1,19 @@
-N = int(input())
+import math
+import os
+import random
+import re
+import sys
 
-if N % 2 != 0:
-    print("Weird")
-else:
-    if N <= 5:
-        print("Not Weird")
-    elif N <= 20:
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    if N % 2 != 0:
         print("Weird")
     else:
-        print("Not Weird")
+        if N <= 5:
+            print("Not Weird")
+        elif N <= 20:
+            print("Weird")
+        else:
+            print("Not Weird")
